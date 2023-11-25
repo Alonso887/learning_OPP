7bd56be6667a5e2fdc530ebcc63dc0c3cdee10d8
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2023-11-24 21:03:47 -0600

    Finished the tutorial, very well explained mostly, i think i'm ready to implement it into a more useful proyect, let's see what can i do

--- Hi.py
+++ Hi.py
@@ -1,32 +1,10 @@
-class objeto:
-    discount_porcentage = .9
-    instaces = []
-    def __init__(self, name: str, price: float, quantity=0):
-        #Arguments validations
-        assert price >= 0,"Price must be equal or greater than 0"
-        assert quantity >= 0, "Quantity must be equal or greater than 0"
+from Telefono import Telefono
 
-        #Arguments asiggnment
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+objeto1 = Telefono("Lentes",420)
 
-        # Extra actions 
-        objeto.instaces.append(self)
+objeto1.increment_price(.35)
+objeto1.discount()
 
-    def calculate_total_price(self):
-        return self.price * self.quantity
-    
-    def discount(self):
-        self.price = self.price * self.discount_porcentage
-    
-    def __repr__(self):
-        return f"Objeto(name= '{self.name}', price= '{self.price}', quantity= '{self.quantity}')"
 
-objeto1 = objeto(name= "Iphone", price= 10000, quantity= 25)
-objeto2 = objeto(name= "Headset", price= 5000, quantity= 49)
-objeto3 = objeto(name= "Violin", price= 600, quantity= 32)
-objeto4 = objeto(name= "Mouse", price= 4000, quantity= 17)
-objeto5 = objeto(name= "Apple", price= 50, quantity= 7)
+print(objeto1.price)
 
-print(objeto.instaces)
