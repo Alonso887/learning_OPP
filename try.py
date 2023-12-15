26163fd525a9c253076d860825145c91d205c37b
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2023-12-15 15:54:53 -0600

    New proyect Twikke, twiter but worst, it works with a DB in Mongo so that's cool

--- try.py
+++ try.py
@@ -1,9 +1,12 @@
-import tkinter as tk
-from tkinter import font
+from pymongo import MongoClient
+import base64
 
-root = tk.Tk()
-# Obtener la lista de fuentes disponibles
-fuentes_disponibles = font.families()
+# Conexión a la base de datos
+client = MongoClient('mongodb+srv://LaEntropia:GwsItEDiNC7Jmaq3@twikker.xkiwhwx.mongodb.net/?retryWrites=true&w=majority')
+db = client['Posts']
+coleccion = db['post_info']
 
-# Imprimir la lista de fuentes
-print(fuentes_disponibles)
+doc = coleccion.find_one({"_id":0})
+
+with open(r'C:\Users\aadri\Desktop\Coding\Repositories\learning_OOP\assets\new.png', 'wb') as image_file:
+    image_file.write(base64.b64decode(doc['image']))
